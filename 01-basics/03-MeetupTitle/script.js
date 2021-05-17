4668f2722614189b474548d8a9b1f3fff6427e74
AnastasMir <nmironovic909@gmail.com>
2021-05-17 22:51:58 +0300

    MeetupTitle

--- 01-basics/03-MeetupTitle/script.js
+++ 01-basics/03-MeetupTitle/script.js
@@ -1,3 +1,26 @@
-// import Vue from './vendor/vue.esm.browser.js';
+ import Vue from './vendor/vue.esm.browser.js';
 
 // Требуется создать Vue приложение
+const app = new Vue({
+    el: '#app',
+    data(){
+        return{
+            countMeetups: [1, 2, 3, 4, 5],
+            pickedId: '',
+            title: ''
+        }
+    },
+    watch: {
+        pickedId(){
+            this.getMetups()
+        }
+
+    },
+    methods: {
+        getMetups() {
+            fetch(`https://course-vue.javascript.ru/api/meetups/${this.pickedId}`)
+            .then(response => response.json())
+            .then(result => this.title = result.title)
+        }
+    }
+})
