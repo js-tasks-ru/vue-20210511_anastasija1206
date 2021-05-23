86c414dff5cd75daae7148c6c40727c7f3d431ed
AnastasMir <nmironovic909@gmail.com>
2021-05-23 15:09:54 +0300

    MeetupInfoComponent

--- 02-components/04-MeetupInfo/MeetupInfo.js
+++ 02-components/04-MeetupInfo/MeetupInfo.js
@@ -1,19 +1,33 @@
 export default {
   name: 'MeetupInfo',
+  props: {
+    organizer: String,
+    place: String,
+    date: Date
+  },
+  computed:{
+    data(){
+      return new Date(this.date).toLocaleString('ru-Ru', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      });
+    }
+  },
 
   template: `
     <ul class="info-list">
       <li>
         <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        {{organizer}}
       </li>
       <li>
         <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{place}}
       </li>
       <li>
         <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time datetime="2020-01-01">{{data}}</time>
       </li>
     </ul>`,
 };
