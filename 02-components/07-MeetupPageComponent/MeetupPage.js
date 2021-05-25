a7e1b5a084718ec466c76b7a33804492117a066c
AnastasMir <nmironovic909@gmail.com>
2021-05-25 20:39:22 +0300

    MeetupPageComponent

--- 02-components/07-MeetupPageComponent/MeetupPage.js
+++ 02-components/07-MeetupPageComponent/MeetupPage.js
@@ -1,10 +1,26 @@
-// import MeetupView from './MeetupView.js';
-// import { MEETUP_ID, fetchMeetup } from './data.js';
+import MeetupView from '../06-MeetupView/MeetupView.js';
+import { MEETUP_ID, fetchMeetup } from './data.js';
 
 const MeetupPage = {
   name: 'MeetupPage',
+  components: {
+    MeetupView
+  },
+  data(){
+    return{
+      meetups: null
+    }
+  },
+  computed: {
+   getMeetup(){
+    return this.meetups
+  }
+  },
+  async mounted() {
+    this.meetups = await fetchMeetup(MEETUP_ID);
+  },
 
-  template: `<div></div>`,
+  template: `<div v-if="meetups"><MeetupView :meetup="getMeetup"></MeetupView></div>`,
 };
 
 export default MeetupPage;
